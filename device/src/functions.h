c23f08e3c69c7704952227d4cbf7a02931ce85b8
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2018-10-17 21:18:13 +0200

    Added suppor for documentation, started refactoring.

--- device/src/functions.h
+++ device/src/functions.h
@@ -1,3 +1,6 @@
-INTERFACE(int, bla, void)
-INTERFACE(int, add, int, int)
-INTERFACE(void, setLed, int)
+INTERFACE("Test function.",
+  int, bla, void)
+INTERFACE("Add two integers. @P:Value a. @P:Value b. @R:Sum of a and b.",
+  int, add, int, int)
+INTERFACE("Set LED intensity. @P:Intensity between 0 and 255.",
+  void, setLed, int)
