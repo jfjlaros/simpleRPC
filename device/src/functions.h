8add6d91c5c635829bd30391ad489ea602b195ea
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2018-10-18 22:03:54 +0200

    Documentation.

--- device/src/functions.h
+++ device/src/functions.h
@@ -1,5 +1,7 @@
-INTERFACE("Test function.",
-  int, bla, void)
+INTERFACE("Test function. @R:The number 1.",
+  int, testInt, void)
+INTERFACE("Test function. @R:The constant phi.",
+  float, testFloat, void)
 INTERFACE("Add two integers. @P:Value a. @P:Value b. @R:Sum of a and b.",
   int, add, int, int)
 INTERFACE("Set LED intensity. @P:Intensity between 0 and 255.",
