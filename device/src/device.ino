f8064111ada7c188c4d606c4dc5bbbd11ccf92fa
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2018-10-19 19:34:33 +0200

    Separated library from sketch.

--- device/src/device.ino
+++ device/src/device.ino
@@ -1,4 +1,4 @@
-#include "simpleRPC.h"
+#include <simpleRPC.h>
 
 
 int testInt(void) {
---
+++ simpleRPC/methods.h
@@ -0,0 +1,8 @@
+INTERFACE("Test function. @R:The number 1.",
+  int, testInt, void)
+INTERFACE("Test function. @R:The constant phi.",
+  float, testFloat, void)
+INTERFACE("Add two integers. @P:Value a. @P:Value b. @R:Sum of a and b.",
+  int, add, int, int)
+INTERFACE("Set LED intensity. @P:Intensity between 0 and 255.",
+  void, setLed, int)
