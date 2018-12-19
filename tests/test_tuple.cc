6152405d3022bcc355a740748140c53468973e14
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2018-12-19 19:45:07 +0100

    Added unit test for tuples.

---
+++ tests/test_tuple.cc
@@ -0,0 +1,31 @@
+#include <catch.hpp>
+
+#include "../simpleRPC/tuple.tcc"
+
+
+TEST_CASE("Direct initialisation", "[tuple]") {
+  Tuple <char, int>t = {'x', 1};
+
+  REQUIRE(t.head == 'x');
+  REQUIRE(t.tail.head == 1);
+}
+
+TEST_CASE("Pack values", "[tuple]") {
+  Tuple <char, int>t = pack('x', 1);
+
+  REQUIRE(t.head == 'x');
+  REQUIRE(t.tail.head == 1);
+}
+
+TEST_CASE("Cast struct to tuple", "[tuple]") {
+  struct S {
+    char c;
+    int i;
+  };
+
+  S s = {'x', 1};
+  Tuple <char, int>t = castStruct<char, int>(s);
+
+  REQUIRE(t.head == 'x');
+  REQUIRE(t.tail.head == 1);
+}
