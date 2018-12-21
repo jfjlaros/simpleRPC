c817eceb4d036cff8ac1cee2cb7d05bfcdedb30a
J.F.J. Laros <J.F.J.Laros@lumc.nl>
2018-12-21 11:59:58 +0100

    Added comments.

--- tests/test_tuple.cc
+++ tests/test_tuple.cc
@@ -6,6 +6,7 @@
 TEST_CASE("Direct initialisation", "[tuple]") {
   Tuple <char, int>t = {'x', 1};
 
+  // Using an initialisation list.
   REQUIRE(t.head == 'x');
   REQUIRE(t.tail.head == 1);
 }
@@ -13,6 +14,7 @@ TEST_CASE("Direct initialisation", "[tuple]") {
 TEST_CASE("Pack values", "[tuple]") {
   Tuple <char, int>t = pack('x', 1);
 
+  // Using the pack() function.
   REQUIRE(t.head == 'x');
   REQUIRE(t.tail.head == 1);
 }
@@ -26,6 +28,7 @@ TEST_CASE("Cast struct to tuple", "[tuple]") {
   S s = {'x', 1};
   Tuple <char, int>t = castStruct<char, int>(s);
 
+  // Using the castStruct() function.
   REQUIRE(t.head == 'x');
   REQUIRE(t.tail.head == 1);
 }
