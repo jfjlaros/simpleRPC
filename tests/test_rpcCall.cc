a1eb69bb66c3c73cb5cb2d4d33efd763a7ce20b0
J.F.J. Laros <J.F.J.Laros@lumc.nl>
2018-12-21 11:59:42 +0100

    Added unit tests for rpcCall.

--- tests/test_rpcCall.cc
+++ tests/test_rpcCall.cc
@@ -13,43 +13,69 @@ TEST_CASE("RPC call function", "[call]") {
     static void i(int, const char *) {}
     static void j(char *, int) {}
     static void k(const char *, int) {}
+    static char *l(void) {
+      return (char *)"xxx";
+    }
+    static const char *m(void) {
+      return "xxx";
+    }
   };
 
+  // Void function.
   Serial.reset();
   rpcCall(S::f);
 
   REQUIRE(Serial.rx == sizeof(int) + sizeof(char));
   REQUIRE(Serial.tx == 0);
 
+  // Non-void function.
   Serial.reset();
   rpcCall(S::g);
 
   REQUIRE(Serial.rx == sizeof(int) + sizeof(char));
   REQUIRE(Serial.tx == sizeof(short int));
 
+  // Second parameter is of type char *.
   Serial.reset();
   rpcCall(S::h);
 
   REQUIRE(Serial.rx == sizeof(int) + 3);
   REQUIRE(Serial.tx == 0);
 
+  // Second parameter is of type const char *.
   Serial.reset();
   rpcCall(S::i);
 
-  REQUIRE(Serial.rx == sizeof(int) + 3);
+  REQUIRE(Serial.rx == 3 + sizeof(int));
   REQUIRE(Serial.tx == 0);
 
+  // First parameter is of type char *.
   Serial.reset();
   rpcCall(S::j);
 
-  REQUIRE(Serial.rx == sizeof(int) + 3);
+  REQUIRE(Serial.rx == 3 + sizeof(int));
   REQUIRE(Serial.tx == 0);
 
+  // First parameter is of type const char *.
   Serial.reset();
   rpcCall(S::k);
 
   REQUIRE(Serial.rx == sizeof(int) + 3);
   REQUIRE(Serial.tx == 0);
+
+  // Return type char *.
+  Serial.reset();
+  rpcCall(S::l);
+
+  REQUIRE(Serial.rx == 0);
+  REQUIRE(Serial.tx == 3 + 1);
+
+  // Return type const char *.
+  Serial.reset();
+  rpcCall(S::m);
+
+  REQUIRE(Serial.rx == 0);
+  REQUIRE(Serial.tx == 3 + 1);
 }
 
 TEST_CASE("RPC call class member functions", "[call]") {
@@ -63,12 +89,14 @@ TEST_CASE("RPC call class member functions", "[call]") {
 
   C c;
 
+  // Void function.
   Serial.reset();
   rpcCall(pack(&c, &C::f));
 
   REQUIRE(Serial.rx == sizeof(int) + sizeof(char));
   REQUIRE(Serial.tx == 0);
 
+  // Non-void function.
   Serial.reset();
   rpcCall(pack(&c, &C::g));
 
