079ec80a081fa93e942996d945c3579d46b613f7
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2019-02-14 19:18:47 +0100

    Updated example.

--- example/device/src/device.ino
+++ example/device/src/device.ino
@@ -20,7 +20,7 @@ void setup(void) {
 
 void loop(void) {
   interface(
-    inc, "inc: Increment a value. @a: Value. @return: a + 1.",
-    setLed, "set_led: Set LED brightness. @brightness: Brightness.",
-    time, "time: Report the system time. @return: System time.");
+    inc, F("inc: Increment a value. @a: Value. @return: a + 1."),
+    setLed, F("set_led: Set LED brightness. @brightness: Brightness."),
+    time, F("time: Report the system time. @return: System time."));
 }
