69e09e698592cf1e54e922797591199d74135285
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2019-03-16 16:47:02 +0100

    Added signature tests.

--- tests/test_signature.cc
+++ tests/test_signature.cc
@@ -23,3 +23,27 @@ TEST_CASE("Class member function pointer types", "[signature]") {
   REQUIRE(signature(&C::f) == "<h: c <f");
   REQUIRE(signature(&C::g) == ": c <f");
 }
+
+TEST_CASE("Tuples", "[signature]") {
+  void (*f)(Tuple <int, char>, float);
+  Tuple <int, char> (*g)(float);
+
+  REQUIRE(signature(f) == ": <ic <f");
+  REQUIRE(signature(g) == "<ic: <f");
+}
+
+TEST_CASE("Objects", "[signature]") {
+  void (*f)(Object <int, char>, float);
+  Object <int, char> (*g)(float);
+
+  REQUIRE(signature(f) == ": [<ic] <f");
+  REQUIRE(signature(g) == "[<ic]: <f");
+}
+
+TEST_CASE("Vectors", "[signature]") {
+  void (*f)(Vector <int>, float);
+  Vector <int> (*g)(float);
+
+  REQUIRE(signature(f) == ": (<i) <f");
+  REQUIRE(signature(g) == "(<i): <f");
+}
