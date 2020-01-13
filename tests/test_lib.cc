d148b3a876639ef2907d8df7f27c25a69e58624e
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2020-01-13 19:25:10 +0100

    Plugin support for tests.

--- tests/test_lib.cc
+++ tests/test_lib.cc
@@ -1,6 +1,13 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
 #include "../src/plugins/hardwareserial/io.h"
 
 HardwareSerialIO io;
+
+
+int main(int argc, char** argv) {
+  io.begin(Serial);
+
+  return Catch::Session().run(argc, argv);
+}
