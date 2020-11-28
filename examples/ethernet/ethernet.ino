f0b47c694e5dafeeeb631a018d3b59d33d742ebb
Chris Flesher <chris.flesher@stoneaerospace.com>
2020-11-27 20:52:33 -0600

    fixup! Added ethernet and wifi examples

--- examples/ethernet/ethernet.ino
+++ examples/ethernet/ethernet.ino
@@ -1,18 +1,36 @@
 #include <simpleRPC.h>
 #include <Ethernet.h>
 
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192, 168, 1, 151);
 EthernetServer server(10000);
 
 byte ping(byte data) {
   return data;
 }
 
+void connectEthernet() {
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+}
+
+void printEthernetStatus(Print& print) {
+  print.print("IP Address: ");
+  print.println(ip);
+  if (Ethernet.linkStatus() == LinkOFF) {
+    print.println("Ethernet cable is not connected.");
+  }
+}
+
 void setup(void) {
   Serial.begin(9600);
-  WiFi.setPins(8,7,4,2);
-  connectWifi(ssid, pass);
+  Ethernet.begin(mac, ip);
+  connectEthernet();
   server.begin();
-  printWifiStatus(Serial);
+  printEthernetStatus(Serial);
 }
 
 void loop(void) {
