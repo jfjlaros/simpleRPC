78da534432c111ba6b426ee522cfdeba4f391ce0
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2021-11-10 22:49:56 +0100

    Patch for RTD error.

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,6 @@ author = 'Jeroen F.J. Laros'
 copyright = '2019, {}'.format(author)
 
 extensions = ['breathe']
-breathe_version = '4.3.1'
 breathe_projects = {'doxygen': 'xml'}
 breathe_default_project = 'doxygen'
 breathe_default_members = ['members']
@@ -18,4 +17,4 @@ html_theme = 'sphinx_rtd_theme'
 
 
 subprocess.call(
-    'pip install breathe=={}; doxygen'.format(breathe_version), shell=True)
+    'pip install doxygen', shell=True)
