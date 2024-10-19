07e9b99537c87331633a198653eb983cdd4c3918
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2024-10-19 12:16:23 +0200

    Readthedocs fix.

--- docs/conf.py
+++ docs/conf.py
@@ -8,12 +8,8 @@ copyright = '2019, {}'.format(author)
 extensions = ['breathe']
 breathe_projects = {'doxygen': 'xml'}
 breathe_default_project = 'doxygen'
-breathe_default_members = ('members')
+breathe_default_members = ('members',)
 
 master_doc = 'index'
 
 highlight_language = 'none'
-html_theme = 'sphinx_rtd_theme'
-
-
-call('doxygen', shell=True)
